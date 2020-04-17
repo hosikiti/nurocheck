acf7f980edd93cddcdf55d3efd87ce62ab104ac9
Kei Oikawa <hosikiti@users.noreply.github.com>
2020-04-17 19:41:12 +0900

    Use chromium binary.

--- checker.js
+++ checker.js
@@ -1,10 +1,13 @@
 const URL = process.env.URL;
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const NoticeMessage = "Nuro光の予約ができるようになったかも！";
 const slack = require("./slack");
+const chromium = require("chromium");
 
 const check = async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: chromium.path,
+  });
   const page = await browser.newPage();
   await page.goto(URL);
   await page.waitFor(3000);
@@ -23,6 +26,8 @@ const check = async () => {
       ) < 0
     ) {
       slack.notify(NoticeMessage);
+    } else {
+      slack.notify("予約不可です orz...");
     }
   }
   await browser.close();
