49df631a2855bb525e7dfbf3a4669eadf9279270
Kei Oikawa <hosikiti@users.noreply.github.com>
2020-04-18 08:11:55 +0900

    add debug info

--- handler.js
+++ handler.js
@@ -5,20 +5,41 @@ const fs = require("fs");
 module.exports = async (event, context) => {
   try {
     const chromium = require("chromium");
-    const { execFile } = require("child_process");
-    // await checker.check();
+    const puppeteer = require("puppeteer-core");
 
-    execFile(chromium.path, ["https://google.com"], (err, stdout, stderr) => {
-      context.status(200).succeed({
-        path: chromium.path,
-        exists: fs.existsSync(chromium.path),
-        stdout: stdout,
-        stderr: stderr,
-        err: err,
-      });
-      return;
-      console.log("Hello Google!", err);
-    });
+    const PUPPETEER_OPTIONS = {
+      headless: true,
+      args: [
+        "--disable-gpu",
+        "--disable-dev-shm-usage",
+        "--disable-setuid-sandbox",
+        "--timeout=30000",
+        "--no-first-run",
+        "--no-sandbox",
+        "--no-zygote",
+        "--single-process",
+        "--proxy-server='direct://'",
+        "--proxy-bypass-list=*",
+        "--deterministic-fetch",
+      ],
+    };
+
+    const browser = await puppeteer.launch(PUPPETEER_OPTIONS);
+
+    // const { execFile } = require("child_process");
+    // // await checker.check();
+
+    // execFile(chromium.path, ["https://google.com"], (err, stdout, stderr) => {
+    //   context.status(200).succeed({
+    //     path: chromium.path,
+    //     exists: fs.existsSync(chromium.path),
+    //     stdout: stdout,
+    //     stderr: stderr,
+    //     err: err,
+    //   });
+    //   return;
+    //   console.log("Hello Google!", err);
+    // });
   } catch (err) {
     context.status(200).succeed({
       status: "Browser failed" + `[path: ${chromium.path}]` + String(err),
