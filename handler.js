656b35aa8094d56b4e89eea3be970a664ae68966
Kei Oikawa <hosikiti@users.noreply.github.com>
2020-04-18 10:39:21 +0900

    for test

--- handler.js
+++ handler.js
@@ -3,17 +3,23 @@ const slack = require("./slack");
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 var getos = require("getos");
+const childProcess = require("child_process");
 
 module.exports = async (event, context) => {
-  getos(function (e, os) {
-    if (e) return console.log(e);
+  const spawn = childProcess.spawn("apk", [
+    "add --no-cache nss freetype freetype-dev harfbuzz ca-certificates ttf-freefont",
+  ]);
 
-    if (context) {
-      context.status(200).succeed(os);
-    } else {
-      console.log("os", os);
-    }
+  spawn.stdout.on("data", (data) => {
+    console.log("STDOUT", data.toString()); // Stream
   });
+  spawn.stderr.on("data", (data) => {
+    console.log("STDERR", data.toString()); // Stream
+  });
+  spawn.on("close", (code) => {
+    console.log("CODE", code);
+  });
+  context.status(200).succeed({ result: true });
 
   // const result = {
   //   status: "Nuro check done!",
