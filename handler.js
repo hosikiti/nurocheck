0cd8decbe611a84504bf9c3e74f86e46d2157a69
Kei Oikawa <hosikiti@users.noreply.github.com>
2020-04-18 10:21:21 +0900

    check os

--- handler.js
+++ handler.js
@@ -2,50 +2,18 @@ const checker = require("./checker");
 const slack = require("./slack");
 const fs = require("fs");
 const puppeteer = require("puppeteer");
+var getos = require("getos");
 
 module.exports = async (event, context) => {
-  try {
-    console.log("LAUNCH CHROME!!!");
+  getos(function (e, os) {
+    if (e) return console.log(e);
 
-    const PUPPETEER_OPTIONS = {
-      // executablePath: chromium.path,
-      // headless: true,
-      args: [
-        "--no-sandbox",
-        "--disable-gpu",
-        "--disable-dev-shm-usage",
-        "--disable-setuid-sandbox",
-        // "--timeout=30000",
-        // "--no-first-run",
-        // "--no-zygote",
-        // "--single-process",
-        // "--proxy-server='direct://'",
-        // "--proxy-bypass-list=*",
-        // "--deterministic-fetch",
-      ],
-    };
-
-    const browser = await puppeteer.launch(PUPPETEER_OPTIONS);
-    const page = await browser.newPage();
-    await page.goto("https://www.google.com");
-    console.log("title", await page.title());
-    process.exit(0);
-    return;
-
-    // const { execFile } = require("child_process");
-
-    // execFile(chromium.path, ["--version"], (err) => {
-    //   console.log("Hello Google!", err);
-    // });
-
-    // // await checker.check();
-    // context.status(200).succeed({
-    //   status: "done",
-    // });
-  } catch (err) {
-    console.error(err);
-    return;
-  }
+    if (context) {
+      context.status(200).succeed(os);
+    } else {
+      console.log("os", os);
+    }
+  });
 
   // const result = {
   //   status: "Nuro check done!",
